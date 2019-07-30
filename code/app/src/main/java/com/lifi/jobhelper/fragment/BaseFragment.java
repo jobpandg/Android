a40e81e06a18664022bb99818cdaec28f570dbc2
IamLIFI <lifi13719174004@gmail.com>
2019-07-30 09:23:42 +0800

    update

---
+++ code/app/src/main/java/com/lifi/jobhelper/fragment/BaseFragment.java
@@ -0,0 +1,33 @@
+package com.lifi.jobhelper.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class BaseFragment extends Fragment {
+    public abstract void initView();
+    public abstract void initData();
+    public abstract void initListener();
+    public void init() {
+        initView();
+        initData();
+        initListener();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+}
--- code/app/src/main/java/com/lifi/jobhelper/fragment/OrderPublishFragment.java
+++ code/app/src/main/java/com/lifi/jobhelper/fragment/OrderPublishFragment.java
@@ -18,6 +18,7 @@ public class OrderPublishFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_order_publish, container, false);
+        View view = inflater.inflate(R.layout.fragment_order_publish, container, false);
+        return view;
     }
 }
